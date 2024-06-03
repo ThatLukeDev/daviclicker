f3a10a94e6b92cdd1d32dc6f1f9936290eaa9f8c
ThatLukeDev <luke@l2j.co.uk>
2024-06-03 19:06:18 +0100

    initial commit

---
+++ autohold/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace autohold
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tester = new System.Windows.Forms.Button();
+            this.enabled = new System.Windows.Forms.CheckBox();
+            this.view = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tester
+            // 
+            this.tester.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tester.Location = new System.Drawing.Point(0, 0);
+            this.tester.Name = "tester";
+            this.tester.Size = new System.Drawing.Size(800, 307);
+            this.tester.TabIndex = 0;
+            this.tester.Text = "Click here";
+            this.tester.UseVisualStyleBackColor = true;
+            this.tester.Click += new System.EventHandler(this.tester_Click);
+            // 
+            // enabled
+            // 
+            this.enabled.AutoSize = true;
+            this.enabled.Location = new System.Drawing.Point(13, 314);
+            this.enabled.Name = "enabled";
+            this.enabled.Size = new System.Drawing.Size(65, 17);
+            this.enabled.TabIndex = 1;
+            this.enabled.Text = "Enabled";
+            this.enabled.UseVisualStyleBackColor = true;
+            // 
+            // view
+            // 
+            this.view.AutoSize = true;
+            this.view.Location = new System.Drawing.Point(13, 338);
+            this.view.Name = "view";
+            this.view.Size = new System.Drawing.Size(45, 13);
+            this.view.TabIndex = 2;
+            this.view.Text = "000 cps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.enabled);
+            this.Controls.Add(this.tester);
+            this.Name = "Form1";
+            this.Text = "click";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tester;
+        private System.Windows.Forms.CheckBox enabled;
+        private System.Windows.Forms.Label view;
+    }
+}
+
---
+++ autohold/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autohold
+{
+    public partial class Form1 : Form
+    {
+        bool autoclicking = false;
+        int interval = 10;
+        IntPtr lParamD = IntPtr.Zero;
+        IntPtr lParamU = IntPtr.Zero;
+
+        public delegate IntPtr HookCallbackDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+
+        const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        const int MOUSEEVENTF_LEFTUP = 0x04;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookCallbackDelegate lpfn, IntPtr wParam, uint lParam);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        const int WH_KEYBOARD_LL = 13;
+        const int WH_MOUSE_LL = 14;
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_KEYUP = 0x0101;
+        const int WM_ALTKEYDOWN = 0x0104;
+        const int WM_ALTKEYUP = 0x0105;
+        const int WM_MOUSEMOVE = 0x0200;
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+        const int WM_MOUSEWHEEL = 0x020A;
+        const int WM_RBUTTONDOWN = 0x0204;
+        const int WM_RBUTTONUP = 0x0205;
+        const int WM_LBUTTONDBLCLK = 0x0203;
+        const int WM_MBUTTONDOWN = 0x0207;
+        const int WM_MBUTTONUP = 0x020;
+
+        HookCallbackDelegate mhcDelegate;
+        IntPtr whllmousehook;
+
+        IntPtr mhandler(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (lParamD == IntPtr.Zero || lParam == lParamD)
+            {
+                if (wParam == (IntPtr)WM_LBUTTONDOWN)
+                {
+                    if (lParamD == IntPtr.Zero)
+                    {
+                        lParamD = lParam;
+                    }
+                    if (enabled.Checked)
+                    {
+                        autoclicking = true;
+                    }
+                }
+            }
+
+            if (lParamU == IntPtr.Zero || lParam == lParamU)
+            {
+                if (wParam == (IntPtr)WM_LBUTTONUP)
+                {
+                    if (lParamU == IntPtr.Zero)
+                    {
+                        lParamU = lParam;
+                    }
+                    autoclicking = false;
+                }
+            }
+
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        public void startListening()
+        {
+            mhcDelegate = mhandler;
+
+            string mainModuleName = Process.GetCurrentProcess().MainModule.ModuleName;
+            whllmousehook = SetWindowsHookEx(WH_MOUSE_LL, mhcDelegate, GetModuleHandle(mainModuleName), 0);
+        }
+        public void endListening()
+        {
+            UnhookWindowsHookEx(whllmousehook);
+            mhcDelegate = null;
+        }
+
+        int cps = 0;
+        int count = 0;
+
+        Timer secondInterval;
+        Timer msInterval;
+
+        private void secondInterval_Tick(object sender, EventArgs e)
+        {
+            cps = count;
+            count = 0;
+            view.Text = string.Format("{0:00} cps", cps);
+        }
+
+        private void msInterval_Tick(object sender, EventArgs e)
+        {
+            if (autoclicking)
+            {
+                Click(5);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            startListening();
+            secondInterval = new Timer();
+            secondInterval.Tick += new EventHandler(secondInterval_Tick);
+            secondInterval.Interval = 1000;
+            secondInterval.Start();
+            msInterval = new Timer();
+            msInterval.Tick += new EventHandler(msInterval_Tick);
+            msInterval.Interval = interval;
+            msInterval.Start();
+        }
+
+        private void tester_Click(object sender, EventArgs e)
+        {
+            count++;
+        }
+
+        private async void Click(int delay)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+            await Task.Delay(delay);
+            mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+        }
+    }
+}
